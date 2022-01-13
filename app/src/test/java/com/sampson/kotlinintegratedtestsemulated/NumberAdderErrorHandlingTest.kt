577ff6f449afb6a7e9d85b99fd6783544929bb2a
fsampson30@ <fsampson30@gmail.com>
2022-01-13 08:15:58 -0300

    Unit test for error handling created.

---
+++ app/src/test/java/com/sampson/kotlinintegratedtestsemulated/NumberAdderErrorHandlingTest.kt
@@ -0,0 +1,23 @@
+package com.sampson.kotlinintegratedtestsemulated
+
+import com.nhaarman.mockitokotlin2.mock
+import junit.framework.TestCase
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.junit.MockitoJUnitRunner
+import java.math.BigInteger
+
+@RunWith(MockitoJUnitRunner::class)
+class NumberAdderErrorHandlingTest {
+    @InjectMocks
+    lateinit var numberAdder: NumberAdder
+    @Test(expected = NumberAdder.InvalidNumberException::class)
+    fun sum(){
+        val input = -1
+        val callback = mock<(BigInteger) -> Unit>()
+        numberAdder.sum(input,callback)
+    }
+}
