77156b1fcefd0b93e7900992b34534b2de6fc670
fsampson30@ <fsampson30@gmail.com>
2022-01-18 07:53:21 -0300

    Refactored MainActivityUiTest.kt to run on the emulator.

--- app/src/androidTest/java/com/sampson/kotlinintegratedtestsemulated/MainActivityUiTest.kt
+++ app/src/androidTest/java/com/sampson/kotlinintegratedtestsemulated/MainActivityUiTest.kt
@@ -5,33 +5,48 @@ import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest {
+class MainActivityUiTest {
+    @JvmField
+    @Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun `show sum result in text view`(){
-        val scenario = launch<MainActivity>(MainActivity::class.java)
-        scenario.moveToState(Lifecycle.State.RESUMED)
-        scenario.onActivity { activity ->
-            onView(withId(R.id.edit_text)).perform(ViewActions.replaceText("5"))
+    fun showSumResultInTextView() {
+        activityRule.activity.let { activity ->
+            onView(withId(R.id.edit_text)).perform(replaceText("5"))
             onView(withId(R.id.button)).perform(click())
-            onView(withId(R.id.text_view)).check(matches(withText(activity.getString(R.string.result,5,"15"))))
+            onView(withId(R.id.text_view)).check(
+                matches(
+                    withText(
+                        activity.getString(
+                            R.string.result,
+                            5,
+                            "15"
+                        )
+                    )
+                )
+            )
         }
+
+
     }
 
     @Test
-    fun `show error in text view`(){
-        val scenario = launch<MainActivity>(MainActivity::class.java)
-        scenario.moveToState(Lifecycle.State.RESUMED)
-        scenario.onActivity { activity ->
+    fun showErrorInTextView() {
+        activityRule.activity.let { activity ->
             onView(withId(R.id.edit_text)).perform(ViewActions.replaceText("-5"))
             onView(withId(R.id.button)).perform(click())
             onView(withId(R.id.text_view)).check(matches(withText(activity.getString(R.string.error))))
